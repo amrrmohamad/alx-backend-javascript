90ae61b9ab9ed34dd442395db3cd0b23b9b9c71f
amrrmohamad <amr.r.mohamad55@gmail.com>
2024-11-27 10:02:16 +0200

    update

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param Message to display.
+ * @author amr mohamad <https://github.com/amrrmohamad>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
